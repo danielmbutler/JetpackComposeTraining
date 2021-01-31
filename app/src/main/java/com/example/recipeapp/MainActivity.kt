2b3fbe55d4081377d3b9d054e10b72438fdce420
danielmbutler <danielmbutler@hotmail.co.uk>
2021-01-31 09:59:11 +0000

    nav component

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -16,61 +16,13 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontSynthesis
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import com.example.recipeapp.fragments.RecipeListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
 
-        setContent{
-            ScrollableColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = Color(0xFFF1F2F2))
-            ) {
-                Image(bitmap = imageFromResource(
-                    res = resources,
-                    resId = R.drawable.happy_meal_small
-                ),
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop,
-                )
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ){
-                        Text(
-                            text = "Happy Meal",
-                            style = TextStyle(
-                                fontSize = TextUnit.Companion.Sp(26)
-                            )
-                        )
-                        Text(text = "$5.99",
-                            style = TextStyle(
-                                color = Color(0xFF85bb65),
-                                fontSize = TextUnit.Companion.Sp(17)
-                            ),
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    }
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "800 Calories",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(17)
-                        ))
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Button(
-                        onClick = {},
-                        modifier = Modifier . align (Alignment.CenterHorizontally)
-                    ) {
-                        Text(text = "Order Now")
-                    }
-
-                }
-            }
-
-            }
-        }
     }
-
+}
