5dc5171d327f690d2c743dd9a581c5b23de738e1
danielmbutler <danielmbutler@hotmail.co.uk>
2021-02-02 11:16:57 +0000

    adding glide

--- app/src/main/java/com/example/recipeapp/presentation/components/RecipeCard.kt
+++ app/src/main/java/com/example/recipeapp/presentation/components/RecipeCard.kt
@@ -9,11 +9,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import com.example.recipeapp.R
 import com.example.recipeapp.domain.model.Recipe
+import com.example.recipeapp.util.DEFAULT_RECIPE_IMAGE
+import com.example.recipeapp.util.loadPicture
+
+
+
 
 @Composable
 fun RecipeCard(
@@ -33,13 +39,17 @@ fun RecipeCard(
     ) {
         Column {
             recipe.featuredImage?.let{ url ->
-                Image(
-                    bitmap = imageResource(id = R.drawable.empty_plate),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .preferredHeight(225.dp),
-                contentScale = ContentScale.Crop
-                )
+                val image = loadPicture(url =  url, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let{ img ->
+                    Image(
+                        bitmap = img.asImageBitmap(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(225.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
             }
             recipe.title?.let {title ->
                 Row(
