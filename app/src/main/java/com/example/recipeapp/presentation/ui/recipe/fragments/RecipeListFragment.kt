431b734bac84eef5cb4126625f73d2b0dfd5309b
danielmbutler <danielmbutler@hotmail.co.uk>
2021-02-02 09:44:25 +0000

    persisting data in viewmodels

--- app/src/main/java/com/example/recipeapp/presentation/ui/recipe/fragments/RecipeListFragment.kt
+++ app/src/main/java/com/example/recipeapp/presentation/ui/recipe/fragments/RecipeListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.recipeapp.presentation.ui.recipe.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,22 +20,37 @@ import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.recipeapp.R
+import com.example.recipeapp.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RecipeListFragment : Fragment() {
 
-    val viewModel: RecipeListViewModel by viewModels()
+    private val viewModel: RecipeListViewModel by viewModels()
 
-
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        println("fragment: ${viewModel.getRandomString()}")
+//        println("fragment: ${viewModel.getToken()}")
+//        println("fragment: ${viewModel.getRepo()}")
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+
         return ComposeView(requireContext()).apply {
             setContent {
+
+                val recipes = viewModel.recipes.value
+
+                for(recipe in recipes){
+                    Log.d(TAG, "onCreateView: ${recipe.title}")
+                }
+
                 Column(modifier = Modifier.padding(16.dp)) {
                     Text(
                         text = "Recipe List",
@@ -45,7 +61,7 @@ class RecipeListFragment : Fragment() {
                     Spacer(modifier = Modifier.padding(10.dp))
                     Button(
                         onClick = {
-                            findNavController().navigate(R.id.viewRecipe)
+                            findNavController().navigate(R.id.recipeFragment)
                         }
                     ) {
                         Text(text = "To Receipe Fragment")
--- app/src/main/java/com/example/recipeapp/presentation/ui/recipe/fragments/RecipeListViewModel.kt
+++ app/src/main/java/com/example/recipeapp/presentation/ui/recipe/fragments/RecipeListViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.recipeapp.presentation.ui.recipe.fragments
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.recipeapp.domain.model.Recipe
 import com.example.recipeapp.repository.RecipeRepository
+import kotlinx.coroutines.launch
 import javax.inject.Named
 
 class RecipeListViewModel
@@ -12,10 +19,17 @@ constructor(
     private val repository: RecipeRepository,
     @Named("auth_token") private val token: String,
 ): ViewModel(){
-    init{
-        println("viewmodel: $random")
-        println("viewmodel: $repository")
-        println("viewmodel: $token")
-    }
+  val recipes: MutableState<List<Recipe>> = mutableStateOf(ArrayList())
 
+
+    init{
+        viewModelScope.launch {
+            val result =  repository.search(
+                token = token,
+                page = 1,
+                query = "chicken",
+            )
+            recipes.value = result
+        }
+    }
 }
