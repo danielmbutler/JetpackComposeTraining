9e8232b614641bf871d6b88be72ef8d1223923f1
danielmbutler <danielmbutler@hotmail.co.uk>
2021-02-03 10:51:02 +0000

    animation intro

--- app/src/main/java/com/example/recipeapp/presentation/components/PulsingDemo.kt
+++ app/src/main/java/com/example/recipeapp/presentation/components/PulsingDemo.kt
@@ -5,10 +5,16 @@ import android.util.Log
 import androidx.compose.animation.core.*
 import androidx.compose.animation.transition
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.unit.dp
@@ -29,6 +35,20 @@ fun PulsingDemo(){
 
     val pulseMagnitude = pulseAnim[pulsePropkey]
     Log.d(TAG, "PulsingDemo: $pulseMagnitude")
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp),
+    ) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .height(pulseMagnitude.dp)
+                .width(pulseMagnitude.dp),
+            imageVector = Icons.Default.Favorite
+        )
+    }
 
     Canvas(
         modifier = Modifier
